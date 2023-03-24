840551f4b1f934a4b4d4397ebbb1c3fcf3c5184d
Matias C <109290276+MeCent@users.noreply.github.com>
2023-03-24 14:49:42 -0300

    .jar correccion

--- src/main/java/com/portfolio/mec/Controller/CProyectos.java
+++ src/main/java/com/portfolio/mec/Controller/CProyectos.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyectos")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://mec-frontend-1b8ac.web.app/","http://localhost:4200"})
 public class CProyectos {
     @Autowired
     SProyectos sProyectos;
