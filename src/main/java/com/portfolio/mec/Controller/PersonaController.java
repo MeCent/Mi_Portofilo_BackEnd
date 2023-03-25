a08b154170a1af190be1387be1c98cb6439b52e3
Matias C <109290276+MeCent@users.noreply.github.com>
2023-03-24 21:34:17 -0300

    persona update

--- src/main/java/com/portfolio/mec/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mec/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,27 @@ public class PersonaController {
     public ResponseEntity<List<Persona>> list(){
         List<Persona> list = personaService.list();
         return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona){
+        if(StringUtils.isBlank(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(personaService.existsByNombre(dtopersona.getNombre())){
+            return new ResponseEntity(new Mensaje("El nombre ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        if(StringUtils.isBlank(dtopersona.getApellido())){
+            return new ResponseEntity(new Mensaje("El apellido es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        if(personaService.existsByNombre(dtopersona.getApellido())){
+            return new ResponseEntity(new Mensaje("El apellido ya existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        Persona persona = new Persona(dtopersona.getNombre(), dtopersona.getApellido(), dtopersona.getDescripcion(), dtopersona.getImg());
+        personaService.save(persona);
+        return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
     }
     
     @GetMapping("/detail/{id}")
